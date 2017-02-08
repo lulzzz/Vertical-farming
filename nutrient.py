1456c7b5594b17f3521164bdf50d94108f5b010b
Ajith Panneerselvam <ajithpanneerselvam20@gmail.com>
2017-02-08 08:28:02 -0600

    Tweaks

--- nutrient.py
+++ nutrient.py
@@ -2,12 +2,17 @@ import graphlab
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-	nutrient = graphlab.SFrame('nutrient.csv') 	# Loading the nutrient.csv file
-	nutrient 									# Display the table
-	graphlab.canvas.set_target('ipynb')		# Set the canvas target to ipython notebook
+	#Loading the nutrient.csv file
+	nutrient = graphlab.SFrame('nutrient.csv')
+	#Display the table
+	nutrient
+	
+	# Set the canvas target to ipython notebook
+	graphlab.canvas.set_target('ipynb')
+	
 	nutrient.show(view="Scatter Plot",x="Potassium",y="Growth rate")
 	train_data,test_data = nutrient.random_split(.6,seed=0)
-	​growth_model = graphlab.linear_regression.create(train_data, target='Growth rate', 
+	growth_model = graphlab.linear_regression.create(train_data, target='Growth rate', 
 		features=['Potassium'],validation_set=None)
 	
 	print test_data['Growth rate'].mean()
